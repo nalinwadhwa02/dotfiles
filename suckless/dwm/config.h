332b1aec3866cf77499e56e3ec6facc314aaf6d4
nalinwadhwa02 <nalin.wadhwa02@gmail.com>
2021-08-28 22:02:32 +0530

    updated

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -88,8 +88,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run","-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+//static const char *dmenucmd[] = { "LC_ALL=en_US.UTF-8","dmenu_run","-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = {"LC_ALL=en_US.UTF-8 ", "dmenu_run", NULL};
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
