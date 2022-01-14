f8a743816e3f30d0324295bdafdea766013548a2
nalinwadhwa02 <nalin.wadhwa02@gmail.com>
2022-01-14 22:38:06 +0530

    made changes

--- autostart.sh
+++ autostart.sh
@@ -3,4 +3,8 @@ setxkbmap -option caps:escape &
 udiskie -t &
 xfce4-power-manager &
 nm-applet &
-picom --no-fading-openclose --fade-in-step=1 --fade-out-step=1 --fade-delta=0
+xcompmgr &
+# picom --no-fading-openclose --fade-in-step=1 --fade-out-step=1 --fade-delta=0 &
+xsetroot -solid "#444444"
+
+# feh --bg-fill /home/nw/Pictures/Backgrounds/ismail-inceoglu-signal-not-found.jpg
